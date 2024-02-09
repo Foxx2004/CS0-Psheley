7bbda1c62f9ae42cc0c5dd03b41738a88590b2bd
Foxx2004 <pfsheley@mavs.coloradomesa.edu>
2024-02-09 16:08:00 -0700

    ggg

--- Assignments/Hello/stdio/stdio.py
+++ Assignments/Hello/stdio/stdio.py
@@ -1,10 +1,11 @@
 '''This program will print 4 different playing cards all with different suits and will also ask for the users name'''
 
 name = input("Please enter your name ")
-print("Hello "+name+" I hope you like my playing cards")
 
 card1= print("=========\n" +"| A     |\n"+"|  /\\/\\ |\n"+"|  \\  / |\n"+"|   \\/  |\n"+"|     A |\n"+"=========\n")
-card2= print("=========\n" +"| A /\  |\n"+"|  /  \ |\n"+"|  \  / |\n"+"|   \/  |\n"+"|     A |\n"+"=========\n")
+card2= print("=========\n"+"| A     |\n" +"|  /\   |\n"+"| /  \  |\n"+"| \  /  |\n"+"|  \/ A |\n"+"=========\n")
 card3= print("=========\n" +"| A     |\n"+"| ,'`.  |\n"+"|(_,._) |\n"+"|  /\   |\n"+"|     A |\n"+"=========\n")
 card4= print("=========\n" +"| A _   |\n"+"| _(_)_ |\n"+"|(_)+(_)|\n"+"|   |   |\n"+"|     A |\n"+"=========\n")
 print(card1, card2, card3, card4)
+
+print("Hello "+name+" I hope you like my playing cards")
